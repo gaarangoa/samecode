cf9855f7236eb075feed85f76d58c4ae49c3bad9
kmvr819 <gustavo.arango@astrazeneca.com>
2023-06-23 20:15:17 +0000

    added install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='samecode',
+    version='1.0.0',
+    author='Gustavo Arango',
+    author_email='',
+    description='everyday tools',
+    packages=['samecode'],
+    install_requires=[
+        # List your module's dependencies here
+    ]
+)
